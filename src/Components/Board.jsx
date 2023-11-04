636597d56765b0daf77ffceb8ffe532467b24af4
gautamV19 <ee20b144@smail.iitm.ac.in>
2023-11-05 00:58:30 +0530

    improved UI

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -7,8 +7,33 @@ import Card from "./Card";
 export default function Board() {
   const [ticket, setTicket] = useState([]);
   const [users, setUsers] = useState([]);
-  const [display, setDisplay] = useState("Status");
-  const [order, setOrder] = useState("Priority");
+  const [display, setDisplay] = useState("");
+  const [order, setOrder] = useState("");
+
+  const columns = ticket.reduce((acc, t) => {
+    const groupByKey =
+      display === "Status"
+        ? "status"
+        : display === "Priority"
+        ? "priority"
+        : "userId";
+
+    const groupByValue = t[groupByKey];
+    acc[groupByValue] = acc[groupByValue] || [];
+    acc[groupByValue].push(t);
+
+    return acc;
+  }, {});
+
+  Object.keys(columns).forEach((key) => {
+    columns[key] = columns[key].sort((a, b) => {
+      if (order === "Priority") {
+        return b.priority - a.priority;
+      } else {
+        return a.title.localeCompare(b.title);
+      }
+    });
+  });
 
   useEffect(() => {
     fetch("https://api.quicksell.co/v1/internal/frontend-assignment")
@@ -16,57 +41,135 @@ export default function Board() {
       .then((data) => {
         setTicket(data.tickets);
         setUsers(data.users);
-        console.log(users);
       });
+
+    setDisplay("Status");
+    setOrder("Priority");
   }, []);
+
+  const _users = users.reduce((acc, u) => {
+    acc[u.id] = { name: u.name, available: u.available };
+    return acc;
+  }, {});
 
   const displayOptions = ["Status", "Priority", "User"];
   const orderOptions = ["Priority", "Title"];
 
+  const [click, setClick] = useState(false);
+
   return (
-    <div>
+    <div
+      onClick={() => {
+        setClick(false);
+      }}
+    >
       <div
         style={{
-          borderBottom: "1px solid black",
-          margin: "5px",
-          height: "5vh",
+          height: "60px",
           position: "sticky",
+          backgroundColor: "white",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "flex-start",
+          paddingLeft: "10px",
         }}
       >
-        <Popup trigger={<button> Display </button>} position="right center">
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-evenly",
-              gap: "15px",
-            }}
-          >
-            <div>Grouping</div>
-            <Dropdown
-              options={displayOptions}
-              onChange={(e) => setDisplay(e.value)}
-              value={display}
-            />
-          </div>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-evenly",
-              gap: "15px",
-            }}
-          >
-            <div>Ordering</div>
-            <Dropdown
-              options={orderOptions}
-              onChange={(e) => setOrder(e.value)}
-              value={order}
-            />
-          </div>
-        </Popup>
+        <button
+          style={{
+            backgroundColor: "white",
+            border: "1px solid #acacac",
+            borderRadius: "5px",
+            cursor: "pointer",
+          }}
+          onClick={(e) => {
+            e.stopPropagation();
+            setClick((prev) => !prev);
+          }}
+        >
+          ≡ Display 🔽{" "}
+        </button>
       </div>
-      {ticket.map((t, i) => {
-        return <Card ticket={t} key={i} users={users} />;
-      })}
+      {click && (
+        <div
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
+          style={{
+            position: "absolute",
+            backgroundColor: "white",
+            border: "1px solid #acacac",
+            borderRadius: "5px",
+            width: "250px",
+            top: "4.5vh",
+            left: "10px",
+            padding: "10px",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              marginBottom: "10px",
+            }}
+          >
+            <span>Grouping</span>
+            <select
+              onChange={(e) => {
+                setDisplay(e.target.value);
+              }}
+            >
+              {displayOptions.map((option) => {
+                return <option>{option}</option>;
+              })}
+            </select>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            <span>Ordering</span>
+            <select
+              onChange={(e) => {
+                setOrder(e.target.value);
+              }}
+            >
+              {orderOptions.map((option) => {
+                return <option>{option}</option>;
+              })}
+            </select>
+          </div>
+        </div>
+      )}
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-around",
+        }}
+      >
+        {Object.keys(columns).map((key) => {
+          return (
+            <div
+              style={{
+                display: "inline-block",
+                width: `${
+                  (90 + Object.keys(columns).length * 1.5) /
+                  Object.keys(columns).length
+                }%`,
+              }}
+            >
+              <div>{key}</div>
+              <div>
+                {columns[key].map((t, key) => {
+                  return <Card ticket={t} users={_users} />;
+                })}
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,8 +1,4 @@
-import React, { useState } from "react";
-import Avatar from "react-avatar";
-import { useEffect } from "react";
-
-import Available from "./Available";
+import React from "react";
 
 import no_priority from "../assets/icons/no_priority.png";
 import low from "../assets/icons/low.png";
@@ -10,52 +6,135 @@ import medium from "../assets/icons/medium.png";
 import high from "../assets/icons/high.png";
 import urgent from "../assets/icons/urgent.png";
 
+import backlog from "../assets/icons/backlog.png";
+import in_progress from "../assets/icons/in_progress.png";
+import todo from "../assets/icons/todo.png";
+import done from "../assets/icons/done.png";
+import canceled from "../assets/icons/canceled.png";
 export default function Card({ ticket, users }) {
-  const [name, setName] = useState("");
-  const [available, setAvailable] = useState(false);
-  const [priority, setpriority] = useState(0);
+  const priority_arr = [no_priority, low, medium, high, urgent];
+  const status_arr = {
+    Backlog: backlog,
+    "In progress": in_progress,
+    Todo: todo,
+    Done: done,
+    Canceled: canceled,
+  };
 
-  const arr = [no_priority, low, medium, high, urgent];
-
-  useEffect(() => {
-    users.forEach((u) => {
-      if (u.id == ticket.userId) {
-        setName(u.name);
-        setAvailable(u.available);
-      }
-    });
-  }, [users]);
-
-  useEffect(() => {
-    setpriority(arr[ticket.priority]);
-  }, [ticket]);
-
-  const ticketIdStyle = {};
+  const name = users[ticket.userId].name;
+  const available = users[ticket.userId].available;
+  const priority = priority_arr[ticket.priority];
+  const status = status_arr[ticket.status];
 
   return (
     <div
       style={{
         display: "flex",
         flexDirection: "column",
-        border: "2px solid black",
-        margin: "2px",
-        padding: "5px",
+        border: "1px solid #acacac",
+        margin: "10px 2px",
+        padding: "15px",
+        backgroundColor: "#fff",
+        borderRadius: "10px",
       }}
     >
       <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <p style={ticketIdStyle}>{ticket.id}</p>
+        <span
+          style={{
+            fontSize: "16px",
+            color: "#9c9c9c",
+          }}
+        >
+          {ticket.id}
+        </span>
         <div>
-          <Avatar name={name} size="40" round="20px" />
-          <Available flag={available} />
+          <div
+            style={{
+              height: "25px",
+              width: "30px",
+              borderRadius: "50%",
+              textAlign: "center",
+              paddingTop: "5px",
+              color: "white",
+              backgroundColor: `rgb(${Math.random() * 255},${
+                Math.random() * 255
+              },${Math.random() * 255})`,
+            }}
+          >
+            {name.trim()[0]}
+          </div>
+          {/* <Available flag={available} /> */}
+          <div
+            style={{
+              marginTop: "-18px",
+              color: `${available ? "green" : "grey"}`,
+              textAlign: "right",
+              fontSize: "20px",
+            }}
+          >
+            &#9679;
+          </div>
         </div>
       </div>
-      <h2>{ticket.title}</h2>
-      {/* <p>{ticket.status}</p> */}
       <div
         style={{ display: "flex", justifyContent: "space-start", gap: "10px" }}
       >
-        <img src={priority} alt={ticket.priority} />
-        <p>{ticket.tag[0]}</p>
+        <img src={status} height={"18px"} width={"18px"} alt={ticket.status} />
+        <span
+          style={{
+            fontSize: "14px",
+            fontWeight: "600",
+            color: "#3c3c3c",
+          }}
+        >
+          {ticket.title}
+        </span>
+      </div>
+
+      {/* <p>{ticket.status}</p> */}
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-start",
+          gap: "10px",
+          alignItems: "center",
+          marginTop: "10px",
+        }}
+      >
+        <img
+          src={priority}
+          height={"18px"}
+          width={"18px"}
+          alt={ticket.priority}
+        />
+        {ticket.tag.map((t) => {
+          return (
+            <span
+              style={{
+                border: "1px solid #acacac",
+                borderRadius: "5px",
+                padding: "0 5px",
+                fontSize: "12px",
+                fontWeight: "500",
+                color: "#acacac",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+                gap: "5px",
+              }}
+            >
+              <span
+                style={{
+                  fontSize: "20px",
+                  fontWeight: "600",
+                }}
+              >
+                &#9679;
+              </span>{" "}
+              {t}
+            </span>
+          );
+        })}
       </div>
     </div>
   );
